a33deac1a89e6fd7830dea57bc5c8e7772ea7aeb
tmrna <r.deans@outlook.com>
2023-05-13 10:27:37 -0400

    Moving away from strategy pattern.

--- Tester.hpp
+++ Tester.hpp
@@ -1,46 +1,12 @@
 #ifndef TESTER_HPP
 #define TESTER_HPP
 
-#include "concreteClassses/strategy/strategyInterface.hpp"
-#include <memory>
+#include <type_traits>
+#include <iostream>
 
 namespace tstHelper{
 
-  class Tester{
   
-    public:
-      explicit Tester(std::unique_ptr<StrategyInterface> && strategy = {}) : 
-        strategy_(std::move(strategy)){}
-  
-      // binds a test concrete class, options are for operations are:
-      // 
-      // Assignment:
-      //  AllAssignment
-      //  Assignment
-      //  MoveAssignment
-      //  TrivialAssignment
-      //
-      // Construction:
-      //  AllConstruction
-      //  Construction
-      //  CopyConstruction
-      //  MoveConstruction
-      void bindTestType(std::unique_ptr<StrategyInterface> && strategy);
-  
-      bool runTest(int argCt) const;
-  
-    private:
-      std::unique_ptr<StrategyInterface> strategy_;
-  };
-
-
-  void Tester::bindTestType(std::unique_ptr<StrategyInterface> && strategy) {
-    strategy_ = std::move(strategy);
-  }
-
-  bool Tester::runTest(int argCt) const {
-    return strategy_->testOperations(argCt);
-  }
 }
 
 #endif
